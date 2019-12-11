3288ee6919b74f577cb75453705457d98d0cb768
13952522076 <1007540910@qq.com>
2019-12-10 20:31:22 -0600

    NA

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 # ResNet
 from .resnet import *
 
+from .mobilenet import *
---
+++ models/mobilenet/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+
+############## ResNet #########################
+# Original
+from .mobilenet_na import *
