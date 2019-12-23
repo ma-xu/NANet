a682d9085745a55d37e6663160d098288ca8527c
13952522076 <xuma@my.unt.edu>
2019-12-23 11:28:21 -0600

    1

--- models/resnet_distribution/resnet_dis2.py
+++ models/resnet_distribution/resnet_dis2.py
@@ -51,9 +51,10 @@ class DisLayer(nn.Module):
         pdf[pdf != pdf] = 0 # Remove NaN due to precision.
 
 
-        """
+
         #Step3: Value embedding
         x_value = x.expand(self.local_num,b,c,w,h).reshape(self.local_num*b,c,w,h)
+        """
         x_value = self.value_embed(x_value).reshape(self.local_num,b,c,w,h).permute(1,2,3,4,0) # shape [b,c,w,h,local_num]
 
 
