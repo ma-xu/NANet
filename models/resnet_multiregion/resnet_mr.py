290232c741b42c337480f5ed62c777ce9b731762
13952522076 <xuma@my.unt.edu>
2019-12-24 20:38:28 -0600

    gc

--- models/resnet_multiregion/resnet_mr.py
+++ models/resnet_multiregion/resnet_mr.py
@@ -39,22 +39,46 @@ class MRLayer(nn.Module):
 
         return x*self.sigmoid(out)
 """
+
 class MRLayer(nn.Module):
-    def __init__(self, channel, reduction = 16):
-        super(MRLayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc       = nn.Sequential(
-                        nn.Linear(channel, channel // reduction),
-                        nn.ReLU(inplace = True),
-                        nn.Linear(channel // reduction, channel),
-                        nn.Sigmoid()
-                )
+    def __init__(self, inplanes, planes):
+        self.conv_mask = nn.Conv2d(inplanes, 1, kernel_size=1)
+        self.softmax = nn.Softmax(dim=2)
+        self.channel_add_conv = nn.Sequential(
+            nn.Conv2d(self.inplanes, self.planes, kernel_size=1),
+            nn.LayerNorm([self.planes, 1, 1]),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.planes, self.inplanes, kernel_size=1)
+        )
 
     def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        return x * y
+        batch, channel, height, width = x.size()
+        input_x = x
+        # [N, C, H * W]
+        input_x = input_x.view(batch, channel, height * width)
+        # [N, 1, C, H * W]
+        input_x = input_x.unsqueeze(1)
+        # [N, 1, H, W]
+        context_mask = self.conv_mask(x)
+        # [N, 1, H * W]
+        context_mask = context_mask.view(batch, 1, height * width)
+        # [N, 1, H * W]
+        context_mask = self.softmax(context_mask)
+        # [N, 1, H * W, 1]
+        context_mask = context_mask.unsqueeze(3)
+        # [N, 1, C, 1]
+        context = torch.matmul(input_x, context_mask)
+        # [N, C, 1, 1]
+        context = context.view(batch, channel, 1, 1)
+
+        channel_add_term = self.channel_add_conv(context)
+
+        out = x
+        out = out + channel_add_term
+
+
+
+
 
 
 def conv3x3(in_planes, out_planes, stride=1):
