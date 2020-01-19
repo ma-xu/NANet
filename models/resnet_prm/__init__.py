383ff9af363e213bc40f9244b0ff324229ec3a18
13952522076 <xuma@my.unt.edu>
2020-01-18 22:26:40 -0600

    prm23s

--- models/resnet_prm/__init__.py
+++ models/resnet_prm/__init__.py
@@ -1,50 +1,51 @@
 from __future__ import absolute_import
 
-from .resnet_PRM import *
-
-from.resnet_PRM50 import *
-
-from .resnet_PRM2 import *
-
-from .resnet_PRM3 import *
-
-from .resnet_PRM4 import *
-
-from .resnet_PRM5 import *
-
-from .resnet_PRM6 import *
-
-from .resnet_PRM7 import *
-
-# remove distance
-from .resnet_PRM8 import *
-
-from .resnet_PRMS import *
-
-
-# distance add e^(-x)
-from .resnet_PRM9 import *
-
-from .resnet_PRM10 import *
-
-from .resnet_PRM11 import *
-
-from .resnet_PRM12 import *
-
-
-from .resnet_PRM10S import *
-from .resnet_PRM10S2 import *
-
-from .resnet_PRM13 import *
-from .resnet_PRM14 import *
-
-from .resnet_PRM15 import *
-from .resnet_PRM15_2 import *
-from .resnet_PRM16 import *
-from .resnet_PRM17 import *
-from .resnet_PRM18 import *
-from .resnet_PRM19 import *
-from .resnet_PRMSS import *
+# from .resnet_PRM import *
+#
+# from.resnet_PRM50 import *
+#
+# from .resnet_PRM2 import *
+#
+# from .resnet_PRM3 import *
+#
+# from .resnet_PRM4 import *
+#
+# from .resnet_PRM5 import *
+#
+# from .resnet_PRM6 import *
+#
+# from .resnet_PRM7 import *
+#
+# # remove distance
+# from .resnet_PRM8 import *
+#
+# from .resnet_PRMS import *
+#
+#
+# # distance add e^(-x)
+# from .resnet_PRM9 import *
+#
+# from .resnet_PRM10 import *
+#
+# from .resnet_PRM11 import *
+#
+# from .resnet_PRM12 import *
+#
+#
+# from .resnet_PRM10S import *
+# from .resnet_PRM10S2 import *
+#
+# from .resnet_PRM13 import *
+# from .resnet_PRM14 import *
+#
+# from .resnet_PRM15 import *
+# from .resnet_PRM15_2 import *
+# from .resnet_PRM16 import *
+# from .resnet_PRM17 import *
+# from .resnet_PRM18 import *
+# from .resnet_PRM19 import *
+# from .resnet_PRMSS import *
 from .resnet_PRM21 import *
 from .resnet_PRM22 import *
 from .resnet_PRM23 import *
+from .resnet_PRM23S import *
